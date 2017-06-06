39bb2bd4f59a164ac94e18be6d3600bb3a432505
Christie Brandao <brandao.7@osu.edu>
2017-06-06 13:37:44 -0400

    Reverse a string manually and using given reverse method

---
+++ src/ReverseString.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Create a method to reverse a string manually (Question from AppAcademy)
+ * This program will also show how to reverse a string using the given reverse method
+ *
+ * Created by Christie on 6/6/2017.
+ */
+public class ReverseString {
+
+    private static String reverseString(String str){
+        String reverse = "";
+        for (int i = str.length()-1; i >= 0; i--){
+            char c = str.charAt(i);
+            reverse = reverse + c;
+        }
+        return reverse;
+    }
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Using manual reverse method ------------------");
+        System.out.println("Enter a string you would like to be reversed: ");
+        String str = in.nextLine();
+        System.out.println("Original string: " + str);
+        String strReversed = reverseString(str);
+        System.out.println("Reversed string: " + strReversed);
+        System.out.println();
+        System.out.println("Using given reverse method -------------------");
+        System.out.println("Original string: " + str);
+        String strReversed2 = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed string: " + strReversed2);
+    }
+}
